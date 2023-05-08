c29e337aa8edb04d7be4362edcddeb779473dbb5
Sarthak Neupane <sarthakrocks2004@gmail.com>
2023-05-08 11:50:57 +0545

    character animations added

--- components/AnimChar.vue
+++ components/AnimChar.vue
@@ -1,3 +1,31 @@
 <template>
-    
+    <div ref="container">
+        <span v-for="char in text">{{ char }}</span>
+    </div>
 </template>
+
+<script setup>
+const props = defineProps({
+    text: {
+        type: String,
+        required: true
+    }
+})
+const { $gsap: gsap } = useNuxtApp();
+
+const container = ref(null);
+
+onMounted(() => {
+    const t1 = gsap.timeline();
+    const characters = container.value.querySelectorAll('span');
+    t1.from(characters, {
+        duration: 1,
+        y: 100,
+        opacity: 0,
+        stagger: 0.1,
+        ease: 'power2.out'
+    })
+})
+
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,24 @@
 <template>
-        <div>
-            <div class="relative">
-                <img v-if="width && width < 1024" class="mb-10" src="/images/ImageThree.jpg" alt="">
-                <div class="absolute top-0 left-0 h-full w-full z-10 bg-gradient-to-b from-dark via-transparent to-dark"></div>
-            </div>
-            <div class="relative z-20 -mt-40 flex flex-col justify-center items-start gap-5 text-7xl font-medium lg:mt-10 lg:w-full lg:flex-row lg:justify-center lg:text-[13rem] lg:font-bold">
-                <div class="text-justify flex justify-center items-center gap-[1px]">
-                    <span>E</span>
-                    <span>N</span>
-                    <span>E</span>
-                    <span>S</span>
-                </div>
-                <div class="flex justify-center items-center gap-[1px]">
-                    <span>Y</span>
-                    <span>Ü</span>
-                    <span>K</span>
-                    <span>S</span>
-                    <span>E</span>
-                    <span>K</span>
-                </div>
-            </div>
-            <div class="my-8 text-sm flex justify-center items-center gap-4 lg:flex-row-reverse" v-if="width >= 1024">
-                <p class="lg:font-medium">Designing And Developing Brands</p>
-                <div class="flex-1 h-[1px] max-h-[1px] bg-light"></div>
-            </div>
-            <div class="mt-7">
-                <h2 class=" text-xl lg:text-2xl lg:font-semibold">Developer From Germany</h2>
-                <p class="mt-3 lg:max-w-md font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto voluptatum ea eum explicabo eaque dolores pariatur aperiam similique voluptates excepturi?</p>
-            </div>
+    <div>
+        <div class="relative">
+            <img v-if="width && width < 1024" class="mb-10" src="/images/ImageThree.jpg" alt="">
+            <div class="absolute top-0 left-0 h-full w-full z-10 bg-gradient-to-b from-dark via-transparent to-dark"></div>
         </div>
+        <div
+            class="relative z-20 -mt-40 flex flex-col justify-center items-start gap-5 text-7xl font-medium lg:mt-10 lg:w-full lg:flex-row lg:justify-center lg:text-[13rem] lg:font-bold">
+            <AnimChar text="ENES" class="text-justify flex justify-center items-center gap-[1px]"> </AnimChar>
+            <AnimChar text="YÜKSEK" class="flex justify-center items-center gap-[1px]"> </AnimChar>
+        </div>
+        <div class="my-8 text-sm flex justify-center items-center gap-4 lg:flex-row-reverse" v-if="width >= 1024">
+            <p class="lg:font-medium">Designing And Developing Brands</p>
+            <div class="flex-1 h-[1px] max-h-[1px] bg-light"></div>
+        </div>
+        <div class="mt-7">
+            <h2 class=" text-xl lg:text-2xl lg:font-semibold">Developer From Germany</h2>
+            <p class="mt-3 lg:max-w-md font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+                voluptatum ea eum explicabo eaque dolores pariatur aperiam similique voluptates excepturi?</p>
+        </div>
+    </div>
 </template>
 
 <script setup>
