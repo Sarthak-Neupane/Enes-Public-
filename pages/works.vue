d62bcb6098bee88a9e0d084fec0a17837c5725c6
Sarthak Neupane <sarthakrocks2004@gmail.com>
2023-05-20 15:09:02 +0545

    slight little animation in the works page

--- pages/works.vue
+++ pages/works.vue
@@ -3,24 +3,28 @@
         <h1 class="text-2xl lg:text-3xl xl:text-5xl py-2 lg:font-semibold overflow-y-hidden">
             <AnimsAnimLines :action="action" :trigger="{}" :duration="1" ease="power2.out" :opacity="0" :y-percent="-100"
                 :animate="true">
-                View All My Works <Icon name="teenyicons:top-right-outline" class="text-center" size="30px" />
+                View All My Works <Icon name="teenyicons:top-right-outline" class="text-center" :size="getIconSize" />
             </AnimsAnimLines>
         </h1>
-        <div class="w-full grid gap-5 grid-cols-1 lg:grid-cols-4">
-            <NuxtLink to="/works" class="cursor-none col-span-4" >
-                <Cards src="/images/Works/ImageThree.jpg" name="Project One" category="Hobby" type="Website" :order="getOrder(1)" @change="changeIcon" @default="defaultIcon" :aspect="aspect">
+        <div class="w-full grid gap-5 grid-cols-1 lg:grid-cols-4" ref="container">
+            <NuxtLink to="/works" class="cursor-none col-span-4 relative" >
+                <div class="absolute top-0 left-0 bg-light w-full h-full z-50 origin-bottom cardOverlay"></div>
+                <Cards src="/images/Works/ImageThree.jpg" name="Project One" category="Hobby" type="Website" :order="getOrder(1)" @change="changeIcon" @default="defaultIcon" :aspect=" width > 768 ? 'aspect-[2.5/1]' : 'aspect-square'">
                 </Cards>
             </NuxtLink>
-            <NuxtLink to="/works" class="cursor-none col-span-4 lg:col-span-2" >
-                <Cards src="/images/Works/ImageTwo.jpg" name="Project One" category="Hobby" type="Website" :order="getOrder(1)" @change="changeIcon" @default="defaultIcon" :aspect="aspect">
+            <NuxtLink to="/works" class="cursor-none col-span-4 lg:col-span-2 relative" >
+                <div class="absolute top-0 left-0 bg-light w-full h-full z-50 origin-bottom cardOverlay"></div>
+                <Cards src="/images/Works/ImageTwo.jpg" name="Project One" category="Hobby" type="Website" :order="getOrder(1)" @change="changeIcon" @default="defaultIcon" :aspect="width > 768 ? 'aspect-square' : 'aspect-square'">
                 </Cards>
             </NuxtLink>
-            <NuxtLink to="/works" class="cursor-none col-span-4 lg:col-span-2" >
-                <Cards src="/images/Works/ImageOne.jpg" name="Project One" category="Hobby" type="Website" :order="getOrder(1)" @change="changeIcon" @default="defaultIcon" :aspect="aspect">
+            <NuxtLink to="/works" class="cursor-none col-span-4 lg:col-span-2 relative" >
+                <div class="absolute top-0 left-0 bg-light w-full h-full z-50 origin-bottom cardOverlay"></div>
+                <Cards src="/images/Works/ImageOne.jpg" name="Project One" category="Hobby" type="Website" :order="getOrder(1)" @change="changeIcon" @default="defaultIcon" :aspect="width > 768 ? 'aspect-square' : 'aspect-square'">
                 </Cards>
             </NuxtLink>
-            <NuxtLink to="/works" class="cursor-none col-span-4 lg:col-span-2" >
-                <Cards src="/images/Works/ImageFour.jpg" name="Project One" category="Hobby" type="Website" :order="getOrder(1)" @change="changeIcon" @default="defaultIcon" :aspect="aspect">
+            <NuxtLink to="/works" class="cursor-none col-span-4 lg:col-span-4 relative" >
+                <div class="absolute top-0 left-0 bg-light w-full h-full z-50 origin-bottom cardOverlay"></div>
+                <Cards src="/images/Works/ImageFour.jpg" name="Project One" category="Hobby" type="Website" :order="getOrder(1)" @change="changeIcon" @default="defaultIcon" :aspect="width > 768 ? 'aspect-[2/1]' : 'aspect-square'">
                 </Cards>
             </NuxtLink>
         </div>
@@ -36,15 +40,28 @@ const { $gsap: gsap, $Flip: Flip } = useNuxtApp();
 
 const { width } = useWindowSize();
 
-const aspect = ref(width.value > 1024 ? 'aspect-video' : 'aspect-square')
-
 const animStore = useHeroAnimStore()
 
 const action = ref(false)
+const ctx = ref()
+ const container = ref(null)
 
 onMounted(()=>{
     animStore.setAnimating(false)
     action.value = true
+    ctx.value = gsap.context((self)=>{
+        const cards = self.selector('.cardOverlay')
+        gsap.to(cards, {
+            scaleY: 0,
+            delay: 0.3,
+            duration: 1.2,
+            ease: 'power4.out',
+            stagger: {
+                each: 0.4,
+                from: 'start'
+            }
+        })
+    }, container.value)
 })
 
 
@@ -66,6 +83,14 @@ const changeIcon = (v) => {
 const defaultIcon = (v)=>{
     emits('default', v)
 }
+
+const getIconSize = computed(()=>{
+    if(width.value > 1024){
+        return '30px'
+    }else{
+        return '20px'
+    }
+})
 
 
 </script>
