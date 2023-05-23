7582f54ddb3a49bc0d25189cfc11958c66ab205a
Sarthak Neupane <sarthakrocks2004@gmail.com>
2023-05-23 18:16:04 +0545

    added the scroll animations in the about page

--- components/About/Headline.vue
+++ components/About/Headline.vue
@@ -1,58 +1,65 @@
 <template>
-    <div class="text-5xl font-normal col-span-9" v-if="width > 1280">
-        <h1 class="w-full py-1 overflow-hidden">
-            <AnimsAnimLines :action="action" :trigger="{}" :duration="1" ease="power2.out" :opacity="0" :y-percent="-100"
-                :animate="true">
-                I combine my experience in product and
-            </AnimsAnimLines>
-        </h1>
-        <h1 class="w-full py-1 overflow-hidden">
-            <AnimsAnimLines :action="action" :trigger="{}" :duration="1" ease="power2.out" :opacity="0" :y-percent="-100"
-                :animate="true">
-                brand to solve problems, tell stories, and create
-            </AnimsAnimLines>
-        </h1>
-        <h1 class="w-full py-1 overflow-hidden">
-            <AnimsAnimLines @complete="revealNav" :action="action" :trigger="{}" :duration="1" ease="power2.out"
-                :opacity="0" :y-percent="-100" :animate="true">
-                compelling experiences.
-            </AnimsAnimLines>
-        </h1>
-    </div>
-    <div class="col-span-12" v-else>
-        <h1 class="w-full text-4xl font-medium overflow-hidden translate-y-1/4 opacity-0" ref="Mobile_Headline">
-            <span class="min-w-max opacity-0 pointer-events-none">Gap</span> I combine my experience in product and brand to
-            solve problems, tell stories, and create compelling experiences.
-        </h1>
-    </div>
+        <div class="text-5xl font-normal col-span-9" v-if="width > 1280">
+            <h1 class="w-full py-1 overflow-hidden">
+                <AnimsAnimLines :action="action" :trigger="{}" :duration="1" ease="power2.out" :opacity="0" :y-percent="-100"
+                    :animate="true">
+                    I combine my experience in product and
+                </AnimsAnimLines>
+            </h1>
+            <h1 class="w-full py-1 overflow-hidden">
+                <AnimsAnimLines :action="action" :trigger="{}" :duration="1" ease="power2.out" :opacity="0" :y-percent="-100"
+                    :animate="true">
+                    brand to solve problems, tell stories, and create
+                </AnimsAnimLines>
+            </h1>
+            <h1 class="w-full py-1 overflow-hidden">
+                <AnimsAnimLines @complete="complete" :action="action" :trigger="{}" :duration="1" ease="power2.out"
+                    :opacity="0" :y-percent="-100" :animate="true">
+                    compelling experiences.
+                </AnimsAnimLines>
+            </h1>
+        </div>
+        <div class="col-span-12 overflow-hidden " v-else>
+            <h1 class="w-full text-4xl font-medium translate-y-1/4 opacity-0" ref="Mobile_Headline">
+                <span class="min-w-max opacity-0 pointer-events-none">Gap</span> I combine my experience in product and brand to
+                solve problems, tell stories, and create compelling experiences.
+            </h1>
+        </div>
 </template>
 
 <script setup>
 import { useWindowSize } from '@vueuse/core';
-import { useHeroAnimStore } from '~/store/heroAnim';
 
 const { $gsap: gsap, $Flip: Flip } = useNuxtApp();
 
 const { width } = useWindowSize();
 
-const animStore = useHeroAnimStore()
+const emits = defineEmits(['complete'])
+const props = defineProps({
+    action: {
+        type: Boolean,
+        default: false
+    }
+})
 
-const action = ref(false)
-
-const revealNav = () => {
-    animStore.setAnimating(false)
+const complete = () => {
+    emits('complete')
 }
 
 const Mobile_Headline = ref(null)
 
-onMounted(() => {
-    action.value = true
-    gsap.to(Mobile_Headline.value, {
-        opacity: 1,
-        duration: 1,
-        y: 0,
-        ease: 'power4.out'
-    })
+watch(() => props.action, (newVal, oldVal) => {
+    if (newVal) {
+        const timeline = gsap.timeline()
+        timeline.to(Mobile_Headline.value, {
+            opacity: 1,
+            y: 0,
+            duration: 0.5,
+            ease: 'power4.out',
+            onComplete: () => {
+                complete()
+            }
+        })
+    }
 })
-
 </script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,42 +1,46 @@
 <template>
-    <section class="pt-32 lg:pt-40 grid grid-cols-12 px-2 lg:px-7">
-        <AboutHeadline class="mb-16 lg:mb-24" />
+    <section class="pt-32 lg:pt-40 grid grid-cols-12 px-2 lg:px-7" ref="container">
+        <AboutHeadline class="mb-16 lg:mb-24" :action="action" @complete="proceedOtherAnims" />
         <div class="col-span-6" v-if="width > 1024">
         </div>
         <div class="col-span-12 lg:col-span-6 mb-24 lg:mb-36">
-            <p class="text-sm max-w-lg opacity-50 mb-8">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
+            <p class="text-sm max-w-lg opacity-0 mb-8" id="About_Paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam
                 voluptate incidunt accusamus neque dolor esse voluptatem molestiae, voluptatum dolorum ullam perspiciatis,
                 culpa dicta nemo commodi. Eos voluptates atque ipsum ipsam.</p>
             <div class="relative">
-                <div class="absolute inset-0 w-full h-full bg-dark rounded-md scale-y-0"></div>
+                <div class="absolute inset-0 w-full h-full bg-dark rounded-md scale-y-1 origin-bottom" id="About_Image"></div>
                 <nuxt-img src="enes/Leonardo_Diffusion_monochrom_avantgarde_complex_website_portfo_1.jpg"
                     class="rounded-md"></nuxt-img>
             </div>
         </div>
         <div class="col-span-12 lg:col-span-4 mb-12">
-            <h1 class="text-2xl">Everything I've Done</h1>
+            <div class="overflow-hidden">
+                <h1 class="text-2xl translate-y-full" id="About_Skills_Headline">Everything I've Done</h1>
+            </div>
         </div>
-        <div class="col-span-12 lg:col-span-8 lg:pt-24 flex flex-wrap mb-24 lg:mb-36 font-light lg:text-lg">
-            <Skills title="Prouct Development" />
-            <Skills title="Motion Graphics" />
-            <Skills title="Brand Design" />
-            <Skills title="Brand Development" />
-            <Skills title="Product Design" />
+        <div class="col-span-12 lg:col-span-8 lg:pt-24 flex flex-wrap mb-24 lg:mb-36 font-light lg:text-lg" id="About_Skills">
+            <Skills title="Prouct Development" class="opacity-0 translate-y-10 About_Skills_Each" />
+            <Skills title="Motion Graphics" class="opacity-0 translate-y-10 About_Skills_Each" />
+            <Skills title="Brand Design" class="opacity-0 translate-y-10 About_Skills_Each" />
+            <Skills title="Brand Development" class="opacity-0 translate-y-10 About_Skills_Each" />
+            <Skills title="Product Design" class="opacity-0 translate-y-10 About_Skills_Each" />
         </div>
         <div class="col-span-12 lg:col-span-4 mb-16">
-            <h1 class="text-2xl">Everywhere I've Worked</h1>
+            <div class="overflow-hidden">
+                <h1 class="text-2xl translate-y-full" id="About_Experiences_Headline">Everywhere I've Worked</h1>
+            </div>
         </div>
-        <div
+        <div id="About_Experiences"
             class="col-span-12 lg:col-span-6 flex justify-start items-center gap-11 lg:gap-56 font-light opacity-75 lg:opacity-100 mb-24 lg:mb-36">
             <div class="flex flex-col gap-12">
-                <Experiences title="2022 - 2024" subtitle="Creative Director Basic" class="w-full"></Experiences>
-                <Experiences title="2019 - 2021" subtitle="Senior Digital Designer R/GA" class="w-full"></Experiences>
-                <Experiences title="2015 - 2016" subtitle="Junior Designer AKQA" class="w-full"></Experiences>
+                <Experiences title="2022 - 2024" subtitle="Creative Director Basic" class="w-full opacity-0 translate-y-10  About_Experiences_Each"></Experiences>
+                <Experiences title="2019 - 2021" subtitle="Senior Digital Designer R/GA" class="w-full opacity-0 translate-y-10  About_Experiences_Each"></Experiences>
+                <Experiences title="2015 - 2016" subtitle="Junior Designer AKQA" class="w-full opacity-0 translate-y-10  About_Experiences_Each"></Experiences>
             </div>
             <div class="flex flex-col gap-12">
-                <Experiences title="2021 - 2022" subtitle="Design Director Play" class="w-full"></Experiences>
-                <Experiences title="2017 - 2018" subtitle="Digital Designer Google" class="w-full"></Experiences>
-                <Experiences title="2011 - 2014" subtitle="Design Intern AKQA" class="w-full"></Experiences>
+                <Experiences title="2021 - 2022" subtitle="Design Director Play" class="w-full opacity-0 translate-y-10  About_Experiences_Each"></Experiences>
+                <Experiences title="2017 - 2018" subtitle="Digital Designer Google" class="w-full opacity-0 translate-y-10  About_Experiences_Each"></Experiences>
+                <Experiences title="2011 - 2014" subtitle="Design Intern AKQA" class="w-full opacity-0 translate-y-10  About_Experiences_Each"></Experiences>
             </div>
         </div>
     </section>
@@ -55,20 +59,87 @@ const animStore = useHeroAnimStore()
 
 const action = ref(false)
 
-const revealNav = () => {
-    animStore.setAnimating(false)
+const proceedOtherAnims = () => {
+    
+    ctx.value = gsap.context((self)=>{
+        const About_Image = self.selector('#About_Image')
+        const About_Paragraph = self.selector('#About_Paragraph')
+        const About_Skills_Headline = self.selector('#About_Skills_Headline')
+        const About_Skills = self.selector('#About_Skills')
+        const About_Experiences_Headline = self.selector('#About_Experiences_Headline')
+        const About_Experiences = self.selector('#About_Experiences')
+        const About_Skills_Each = self.selector('.About_Skills_Each')
+        const About_Experiences_Each = self.selector('.About_Experiences_Each')
+
+        const timeline = gsap.timeline()
+
+        timeline.to(About_Paragraph, {
+            duration: 1,
+            opacity: 0.5,
+        })
+
+        timeline.to(About_Image, {
+            duration: 1,
+            scaleY: 0,
+            ease: 'power4.inOut',
+            onStart: () => {
+                animStore.setAnimating(false)
+            }
+        }, '-=1')
+
+        gsap.to(About_Skills_Headline, {
+            duration: 1,
+            y: 0,
+            ease: 'power4.out',
+            scrollTrigger: {
+                trigger: About_Skills_Headline,
+                start: 'top 70%',
+            }
+        })
+
+        gsap.to(About_Skills_Each, {
+            duration: 1,
+            y: 0,
+            opacity: 1,
+            ease: 'power4.out',
+            stagger: 0.1,
+            scrollTrigger: {
+                trigger: About_Skills,
+                start: 'top 70%',
+            }
+        })
+
+        gsap.to(About_Experiences_Headline, {
+            duration: 1,
+            y: 0,
+            ease: 'power4.out',
+            scrollTrigger: {
+                trigger: About_Experiences_Headline,
+                start: 'top 70%',
+            }
+        })
+
+        gsap.to(About_Experiences_Each, {
+            duration: 1,
+            y: 0,
+            opacity: 1,
+            ease: 'power4.out',
+            stagger: 0.1,
+            scrollTrigger: {
+                trigger: About_Experiences,
+                start: 'top 70%',
+            }
+        })
+
+
+    }, container.value)
 }
 
-const Mobile_Headline = ref(null)
+const ctx = ref()
+const container = ref()
 
 onMounted(() => {
     action.value = true
-    gsap.to(Mobile_Headline.value, {
-        opacity: 1,
-        duration: 1,
-        y: 0,
-        ease: 'power4.out'
-    })
 })
 
 </script>
